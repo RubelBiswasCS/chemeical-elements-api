bf251d644dc92f628d723b7925da7501b8b56c7e
RubelBiswasCS <rubelbiswas36@gmail.com>
2021-12-24 21:17:43 +0600

    added Elements model

--- base/models.py
+++ base/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Elements(models.Model):
+    name = models.CharField(max_length=30,unique=True,blank=False,null=False)
+    atomic_number = models.IntegerField(unique=True,blank=False,null=False)
+    symbol = models.CharField(max_length=250,blank=False,null=False)
+    group = models.IntegerField(blank=True,null=True)
+    period = models.IntegerField(blank=True,null=True)
+    atomic_weight = models.FloatField(blank=False,null=False)
+    melting_point = models.IntegerField(blank=False,null=False)
+    boiling_point = models.IntegerField(blank=False,null=False)
+    electron_configuration = models.CharField(max_length=250,blank=False,null=False)
