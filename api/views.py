63e8961cbb2c058c192e6895f2e935e9b5239c41
RubelBiswasCS <rubelbiswas36@gmail.com>
2021-12-25 14:44:12 +0600

    added filters

--- api/views.py
+++ api/views.py
@@ -1,10 +1,16 @@
 from base.models import Elements
 from .serializers import ElementsSerializer
 from rest_framework import generics
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import filters
 
 class ElementsList(generics.ListCreateAPIView):
     queryset = Elements.objects.all()
     serializer_class = ElementsSerializer
+    filter_backends = [DjangoFilterBackend,filters.OrderingFilter,filters.SearchFilter]
+    filterset_fields = ['group',]
+    ordering_fields = ['group','atomic_number','atomic_weight',]
+    search_fields = ['=name','=symbol',]
 
 class ElementsDetails(generics.RetrieveUpdateDestroyAPIView):
     queryset = Elements.objects.all()
