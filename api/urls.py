4b31743e9e3b0449d8e17dbcc4e4b31050528775
RubelBiswasCS <rubelbiswas36@gmail.com>
2021-12-25 15:16:16 +0600

    modified urls

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from . import views
 
+app_name = 'api'
 urlpatterns = [
-    path('elements/', views.ElementsList.as_view()),
-    path('elements/<int:pk>/', views.ElementsDetails.as_view()),
+    path('elements/', views.ElementsList.as_view(),name='elements'),
+    path('elements/<int:pk>/', views.ElementsDetails.as_view(),name='elementsdetails'),
 ]
